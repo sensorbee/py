e1a922f4dd7a3536f223557dbf5ab64c5167badf
Shuzo Kashihara <kashihara@preferred.jp>
2015-09-07 18:50:51 +0900

    Improve py2go

--- py/py2go_converter.go
+++ py/py2go_converter.go
@@ -20,7 +20,35 @@ func fromPyTypeObject(o *C.PyObject) data.Value {
 		charPtr := C.PyByteArray_AsString(bytePtr)
 		l := C.PyByteArray_Size(o)
 		return data.Blob(C.GoBytes(unsafe.Pointer(charPtr), C.int(l)))
+
+	case o.ob_type == &C.PyString_Type:
+		size := C.int(C.PyString_Size(o))
+		charPtr := C.PyString_AsString(o)
+		return data.String(string(C.GoBytes(unsafe.Pointer(charPtr), size)))
+
+	case o.ob_type == &C.PyDict_Type:
+		return fromPyMap(o)
+
 	}
 	// TODO: implement other types
+
 	return nil
+}
+
+func fromPyMap(o *C.PyObject) data.Map {
+	m := data.Map{}
+
+	var key, value *C.PyObject
+	pos := C.Py_ssize_t(C.int(0))
+
+	for C.int(C.PyDict_Next(o, &pos, &key, &value)) > 0 {
+		// data.Map's key is only allowed string
+		if key.ob_type != &C.PyString_Type {
+			continue
+		}
+		key := fromPyTypeObject(key).String()
+		m[key] = fromPyTypeObject(value)
+	}
+
+	return m
 }
