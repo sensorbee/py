fc44378ebb588dfaf9743732b71659cf62d43727
TANAKA Daisuke <tanakad@preferred.jp>
2015-09-08 10:17:29 +0900

    modify values test

--- py/_test_go2py.py
+++ py/_test_go2py.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
 
-def go2py(arg):
+def go2py_tostr(arg):
+    return str(arg)
+
+
+def go2py_mapinmap(arg):
     ret = arg['string']
-    ret += '_' + str(arg['int'])
-    ret += '_' + str(arg['float'])
-    ret += '_' + str(arg['byte'])
-    ret += '_' + str(arg['bool'])
-    ret += '_' + str(arg['null'])
+    ret += '_' + arg['map']['instr']
     for v in arg['array']:
         ret += '_' + str(v)
+    return ret
+
+
+def go2py_arrayinmap(arg):
+    ret = arg[0][0]
+    ret += '_' + str(arg[0][1])
+    ret += '_' + arg[1]['map']
     return ret
--- py/go2py_converter_test.go
+++ py/go2py_converter_test.go
@@ -1,6 +1,7 @@
 package py
 
 import (
+	"fmt"
 	. "github.com/smartystreets/goconvey/convey"
 	"pfi/sensorbee/sensorbee/data"
 	"testing"
@@ -19,22 +20,61 @@ func TestConvertGo2PyObject(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(mdl, ShouldNotBeNil)
 
-		Convey("When a object is full value set map", func() {
+		type argAndExpected struct {
+			arg      data.Value
+			expected string
+		}
+
+		Convey("When set an object", func() {
+			values := map[string]argAndExpected{
+				"string": argAndExpected{data.String("test"), "test"},
+				"int":    argAndExpected{data.Int(9), "9"},
+				"float":  argAndExpected{data.Float(0.9), "0.9"},
+				"byte":   argAndExpected{data.Blob([]byte("ABC")), "ABC"},
+				"true":   argAndExpected{data.True, "True"},
+				"false":  argAndExpected{data.False, "False"},
+				"null":   argAndExpected{data.Null{}, "None"},
+			}
+			for k, v := range values {
+				msg := fmt.Sprintf("Then function should return string value: %v", k)
+				Convey(msg, func() {
+					actual, err := mdl.Call("go2py_tostr", v.arg)
+					So(err, ShouldBeNil)
+					So(actual, ShouldEqual, v.expected)
+				})
+			}
+		})
+
+		Convey("When set map in map and map in array", func() {
 			arg := data.Map{
 				"string": data.String("test"),
-				"int":    data.Int(5),
-				"float":  data.Float(0.1),
-				"byte":   data.Blob([]byte("ABC")),
-				"bool":   data.True,
-				"null":   data.Null{},
+				"map": data.Map{
+					"instr": data.String("test2"),
+				},
 				"array": data.Array{
 					data.String("array-test"), data.Int(55),
 				},
 			}
-			actual, err := mdl.Call("go2py", arg)
+			actual, err := mdl.Call("go2py_mapinmap", arg)
 			Convey("Then function should return valid values", func() {
 				So(err, ShouldBeNil)
-				So(actual, ShouldEqual, "test_5_0.1_ABC_True_None_array-test_55")
+				So(actual, ShouldEqual, "test_test2_array-test_55")
+			})
+		})
+
+		Convey("When set array in array and map", func() {
+			arg := data.Array{
+				data.Array{
+					data.String("test"), data.Int(55),
+				},
+				data.Map{
+					"map": data.String("inmap"),
+				},
+			}
+			actual, err := mdl.Call("go2py_arrayinmap", arg)
+			Convey("Then function should return valid values", func() {
+				So(err, ShouldBeNil)
+				So(actual, ShouldEqual, "test_55_inmap")
 			})
 		})
 	})
