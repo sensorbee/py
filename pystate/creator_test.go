2b3f7e80c1bbeb7f93419d85aadef38499314356
TANAKA Daisuke <tanakad@preferred.jp>
2015-10-26 13:01:57 +0900

    add convey test to confirm pystate UDS/UDF

---
+++ pystate/creator_test.go
@@ -0,0 +1,124 @@
+package pystate
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"pfi/sensorbee/sensorbee/core"
+	"pfi/sensorbee/sensorbee/data"
+	"testing"
+)
+
+func TestCreateState(t *testing.T) {
+	cc := &core.ContextConfig{}
+	ctx := core.NewContext(cc)
+	Convey("Given a pystate creator", t, func() {
+		ct := Creator{}
+		Convey("When the parameter has all required values", func() {
+			params := data.Map{
+				"module_name": data.String("_test_creator_module"),
+				"class_name":  data.String("TestClass"),
+			}
+			Convey("Then the state should be created and set default value", func() {
+				state, err := ct.CreateState(ctx, params)
+				So(err, ShouldBeNil)
+				Reset(func() {
+					state.Terminate(ctx)
+				})
+				ps, ok := state.(*PyState)
+				So(ok, ShouldBeTrue)
+				So(ps.modulePath, ShouldEqual, "")
+				So(ps.moduleName, ShouldEqual, "_test_creator_module")
+				So(ps.writeFuncName, ShouldEqual, "")
+
+				t := &core.Tuple{}
+				err = ps.Write(ctx, t)
+				So(err, ShouldNotBeNil)
+
+				ctx.SharedStates.Add("creator_test", "creator_test", state)
+				Reset(func() {
+					ctx.SharedStates.Remove("creator_test")
+				})
+				Convey("When prepare to be called one instance method", func() {
+					Convey("Then exist instance method should be called", func() {
+						dt := data.String("test")
+						v, err := Func(ctx, "creator_test", "write", dt)
+						So(err, ShouldBeNil)
+						So(v, ShouldEqual, `called! arg is "test"`)
+					})
+					Convey("Then not exist instance method should not be called and return error", func() {
+						_, err = Func(ctx, "creator_test", "not_exist_method")
+						So(err, ShouldNotBeNil)
+					})
+				})
+			})
+		})
+
+		Convey("When the parameter has constructor arguments", func() {
+			params := data.Map{
+				"module_name": data.String("_test_creator_module"),
+				"class_name":  data.String("TestClass2"),
+				"v1":          data.String("init_test"),
+			}
+			Convey("Then the state should be created with constructor arguments", func() {
+				state, err := ct.CreateState(ctx, params)
+				So(err, ShouldBeNil)
+				Reset(func() {
+					state.Terminate(ctx)
+				})
+
+				ctx.SharedStates.Add("creator_test2", "creator_test2", state)
+				Reset(func() {
+					ctx.SharedStates.Remove("creator_test2")
+				})
+				v, err := Func(ctx, "creator_test2", "confirm")
+				So(err, ShouldBeNil)
+				So(v, ShouldEqual, `constructor init arg is "init_test"`)
+			})
+		})
+
+		Convey("When the parameter has required value and option value", func() {
+			params := data.Map{
+				"module_name":  data.String("_test_creator_module"),
+				"class_name":   data.String("TestClass"),
+				"write_method": data.String("write"),
+			}
+			Convey("Then the state should be created and it is writable", func() {
+				state, err := ct.CreateState(ctx, params)
+				So(err, ShouldBeNil)
+				Reset(func() {
+					state.Terminate(ctx)
+				})
+				ps, ok := state.(*PyState)
+				So(ok, ShouldBeTrue)
+				So(ps.writeFuncName, ShouldEqual, "write")
+
+				t := &core.Tuple{}
+				err = ps.Write(ctx, t)
+				So(err, ShouldBeNil)
+			})
+		})
+
+		Convey("When the parameter lacks module name", func() {
+			params := data.Map{
+				"class_name": data.String("TestClass"),
+			}
+			Convey("Then a state should not be created", func() {
+				state, err := ct.CreateState(ctx, params)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, "module_name")
+				So(state, ShouldBeNil)
+			})
+		})
+
+		Convey("When the parameter lacks class name", func() {
+			params := data.Map{
+				"module_name": data.String("_test_creator_module"),
+			}
+			Convey("Then a state should not be created", func() {
+				state, err := ct.CreateState(ctx, params)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, "class_name")
+				So(state, ShouldBeNil)
+			})
+		})
+	})
+}
