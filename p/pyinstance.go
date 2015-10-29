7472491773ec7b735efed7f2656a6ca03608361c
TANAKA Daisuke <tanakad@preferred.jp>
2015-10-29 16:39:15 +0900

    support named arguments
    unit test is not made yet, will be committed after.
    line size is in 80 charactor.

--- p/pyinstance.go
+++ p/pyinstance.go
@@ -1,10 +1,17 @@
 package p
 
+/*
+#include "Python.h"
+*/
+import "C"
 import (
+	"fmt"
 	"pfi/sensorbee/sensorbee/data"
+	"runtime"
+	"unsafe"
 )
 
-// ObjectInstance is a bind of binding Python instance, used as `PyInstance`.
+// ObjectInstance is a bind of Python instance, used as `PyInstance`.
 type ObjectInstance struct {
 	Object
 }
@@ -12,7 +19,8 @@ type ObjectInstance struct {
 // Call calls `name` function.
 //  argument type: ...data.Value
 //  return type:   data.Value
-func (ins *ObjectInstance) Call(name string, args ...data.Value) (data.Value, error) {
+func (ins *ObjectInstance) Call(name string, args ...data.Value) (data.Value,
+	error) {
 	return invoke(ins.p, name, args...)
 }
 
@@ -20,7 +28,64 @@ func (ins *ObjectInstance) Call(name string, args ...data.Value) (data.Value, er
 //  argument type: ...data.Value
 //  return type:   Object.
 //
-// This method is suitable for getting the instance object that called method returned.
-func (ins *ObjectInstance) CallDirect(name string, args ...data.Value) (Object, error) {
+// This method is suitable for getting the instance object that called method
+// returned.
+func (ins *ObjectInstance) CallDirect(name string, args ...data.Value) (Object,
+	error) {
 	return invokeDirect(ins.p, name, args...)
+}
+
+func newInstance(m *ObjectModule, name string, args ...data.Value) (
+	ObjectInstance, error) {
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	type Result struct {
+		val ObjectInstance
+		err error
+	}
+	ch := make(chan *Result, 1)
+	go func() {
+		runtime.LockOSThread()
+		state := GILState_Ensure()
+		defer GILState_Release(state)
+
+		pyInstance := C.PyObject_GetAttrString(m.p, cName)
+		if pyInstance == nil {
+			ch <- &Result{ObjectInstance{}, fmt.Errorf(
+				"cannot create '%v' instance", name)}
+			return
+		}
+		defer C.Py_DecRef(pyInstance)
+
+		pyArg := C.PyTuple_New(C.Py_ssize_t(len(args)))
+		if pyArg == nil {
+			ch <- &Result{ObjectInstance{}, getPyErr()}
+			return
+		}
+		defer C.Py_DecRef(pyArg)
+
+		for i, v := range args {
+			o, err := newPyObj(v)
+			if err != nil {
+				ch <- &Result{ObjectInstance{}, fmt.Errorf("%v at '%v'",
+					err.Error(), name)}
+				return
+			}
+			C.PyTuple_SetItem(pyArg, C.Py_ssize_t(i), o.p)
+		}
+
+		// get constructor (called `__init__(self)`)
+		ret := C.PyObject_Call(pyInstance, pyArg, nil)
+		if ret == nil {
+			ch <- &Result{ObjectInstance{}, fmt.Errorf(
+				"cannot create '%v' instance", name)}
+			return
+		}
+		ch <- &Result{ObjectInstance{Object{p: ret}}, nil}
+	}()
+	res := <-ch
+
+	return res.val, res.err
 }
--- p/pymodule.go
+++ p/pymodule.go
@@ -34,7 +34,8 @@ func LoadModule(name string) (ObjectModule, error) {
 
 		pyMdl, err := C.PyImport_ImportModule(cModule)
 		if pyMdl == nil {
-			ch <- &Result{ObjectModule{}, fmt.Errorf("cannot load '%v' module: %v", name, err)}
+			ch <- &Result{ObjectModule{}, fmt.Errorf("cannot load '%v' module: %v",
+				name, err)}
 			return
 		}
 
@@ -46,54 +47,15 @@ func LoadModule(name string) (ObjectModule, error) {
 }
 
 // NewInstance returns `name` constructor.
-func (m *ObjectModule) NewInstance(name string, args ...data.Value) (ObjectInstance, error) {
-	cName := C.CString(name)
-	defer C.free(unsafe.Pointer(cName))
+func (m *ObjectModule) NewInstance(name string, args ...data.Value) (
+	ObjectInstance, error) {
+	return newInstance(m, name, args...)
+}
 
-	type Result struct {
-		val ObjectInstance
-		err error
-	}
-	ch := make(chan *Result, 1)
-	go func() {
-		runtime.LockOSThread()
-		state := GILState_Ensure()
-		defer GILState_Release(state)
-
-		pyInstance := C.PyObject_GetAttrString(m.p, cName)
-		if pyInstance == nil {
-			ch <- &Result{ObjectInstance{}, fmt.Errorf("cannot create '%v' instance", name)}
-			return
-		}
-		defer C.Py_DecRef(pyInstance)
-
-		pyArg := C.PyTuple_New(C.Py_ssize_t(len(args)))
-		if pyArg == nil {
-			ch <- &Result{ObjectInstance{}, getPyErr()}
-			return
-		}
-		defer C.Py_DecRef(pyArg)
-
-		for i, v := range args {
-			o, err := newPyObj(v)
-			if err != nil {
-				ch <- &Result{ObjectInstance{}, fmt.Errorf("%v at '%v'", err.Error(), name)}
-				return
-			}
-			C.PyTuple_SetItem(pyArg, C.Py_ssize_t(i), o.p)
-		}
-
-		// get constructor (called `__init__(self)`)
-		ret := C.PyObject_CallObject(pyInstance, pyArg)
-		if ret == nil {
-			ch <- &Result{ObjectInstance{}, fmt.Errorf("cannot create '%v' instance", name)}
-			return
-		}
-		ch <- &Result{ObjectInstance{Object{p: ret}}, nil}
-	}()
-	res := <-ch
-
-	return res.val, res.err
+// NewInstanceWithKwd returns 'name' constructor with named arguments.
+func (m *ObjectModule) NewInstanceWithKwd(name string, kwdArgs data.Map) (
+	ObjectInstance, error) {
+	return newInstance(m, name, kwdArgs)
 }
 
 // GetClass returns `name` class instance.
@@ -114,7 +76,8 @@ func (m *ObjectModule) GetClass(name string) (ObjectInstance, error) {
 
 		pyInstance := C.PyObject_GetAttrString(m.p, cName)
 		if pyInstance == nil {
-			ch <- &Result{ObjectInstance{}, fmt.Errorf("cannot get '%v' instance", name)}
+			ch <- &Result{ObjectInstance{}, fmt.Errorf("cannot get '%v' instance",
+				name)}
 			return
 		}
 		ch <- &Result{ObjectInstance{Object{p: pyInstance}}, nil}
